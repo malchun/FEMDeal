b90553f63b4beb69733d7e922e5adfca228383a8
malchun <zmalchunz@gmail.com>
2016-04-18 15:07:11 +0300

    Added loading from file

--- include/GridLoader.h
+++ include/GridLoader.h
@@ -1,4 +1,5 @@
 #include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_in.h>
 
 #include <iostream>
 
@@ -6,7 +7,8 @@ class GridLoader
 {
 private:
 	dealii::Triangulation<3>* triangulation_pointer;
+	std::map <std::string, dealii::GridIn<3>::Format> format_map;
 public:
 	GridLoader(dealii::Triangulation<3>* triangulation);
-	void loadGridFromFile(std::string filename);
+	void loadGridFromFile(std::string filename, std::string format);
 };
--- src/GridLoader.cpp
+++ src/GridLoader.cpp
@@ -1,14 +1,29 @@
 #pragma once
 #include "../include/GridLoader.h"
 
+#include <fstream>
+
+void initialise_format_map(std::map <std::string, dealii::GridIn<3>::Format>* format_map)
+{
+	(*format_map)["msh"] = dealii::GridIn<3>::Format::msh;
+	(*format_map)["vtk"] = dealii::GridIn<3>::Format::vtk;
+	(*format_map)[""] = dealii::GridIn<3>::Format::Default;
+	return;
+}
+
 GridLoader::GridLoader(dealii::Triangulation<3>* triangulation)
 {
 	std::cout << "GridLoader::GridLoader(Triangulation<3>)" << std::endl;
 	this->triangulation_pointer = triangulation;
+	initialise_format_map(&format_map);
 }
 
-void GridLoader::loadGridFromFile(std::string filename)
+void GridLoader::loadGridFromFile(std::string filename, std::string format)
 {
 	std::cout << "GridLoader::loadGridFromFile(\"" << filename << "\")" << std::endl;
+	dealii::GridIn<3> grid_in;
+	grid_in.attach_triangulation (*triangulation_pointer);
+	grid_in.read(filename, format_map[format]);
 	return;
 }
+
